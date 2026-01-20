e00d81bd9d85de62124d4a45c66e6f162b04b837
danimelenge <danielmelenge@gmail.com>
2026-01-20 16:15:53 -0500

    feat: implement SwiftData for history, add HistoryListView and polish conversion button UI

---
+++ Temperature converter/Models/ConversionHistory.swift
@@ -0,0 +1,8 @@
+//
+//  ConversionHistory.swift
+//  Temperature converter
+//
+//  Created by Daniel Melenge Rojas on 20/01/26.
+//
+
+import Foundation
---
+++ Temperature converter/Views/HistoryListView.swift
@@ -0,0 +1,8 @@
+//
+//  HistoryListView.swift
+//  Temperature converter
+//
+//  Created by Daniel Melenge Rojas on 20/01/26.
+//
+
+import Foundation
