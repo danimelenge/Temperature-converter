45914d04c77fc8aef9b4455224ad0590e9c24896
danimelenge <danielmelenge@gmail.com>
2026-05-21 12:33:43 -0500

    feat(widget): implementar WidgetExtension con diseño e interfaz adaptativa

---
+++ TemperatureWidget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  TemperatureWidget
+//
+//  Created by Daniel Melenge Rojas on 21/05/26.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource = "Configuration"
+    static var description = IntentDescription("This is an example widget.")
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
---
+++ TemperatureWidget/TemperatureWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  TemperatureWidgetBundle.swift
+//  TemperatureWidget
+//
+//  Created by Daniel Melenge Rojas on 21/05/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct TemperatureWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        TemperatureWidget()
+    }
+}
